32aefe4d9335a3e16f50e4df01660e8bc0b856cc
zenlord22 <connorlonergan@gmail.com>
2024-11-30 19:15:02 -0600

    Update Program.cs

--- Number Sort/Program.cs
+++ Number Sort/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class Program
 {
@@ -18,6 +18,14 @@ class Program
         int[] bubbleSorted = (int[])numbers.Clone();
         BubbleSort(bubbleSorted);
         Console.WriteLine("Bubble Sorted: " + string.Join(", ", bubbleSorted));
+
+        int[] quickSorted = (int[])numbers.Clone();
+        QuickSort(quickSorted, 0, quickSorted.Length - 1);
+        Console.WriteLine("Quick Sorted: " + string.Join(", ", quickSorted));
+
+        int[] mergeSorted = (int[])numbers.Clone();
+        MergeSort(mergeSorted, 0, mergeSorted.Length - 1);
+        Console.WriteLine("Merge Sorted: " + string.Join(", ", mergeSorted));
     }
 
     static int[] GenerateRandomNumbers(int count)
@@ -80,6 +88,96 @@ class Program
                     array[j + 1] = temp;
                 }
             }
+        }
+    }
+
+    static void QuickSort(int[] array, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = Partition(array, low, high);
+
+            QuickSort(array, low, pi - 1);
+            QuickSort(array, pi + 1, high);
+        }
+    }
+
+    static int Partition(int[] array, int low, int high)
+    {
+        int pivot = array[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++)
+        {
+            if (array[j] < pivot)
+            {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp1 = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp1;
+
+        return i + 1;
+    }
+
+    static void MergeSort(int[] array, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = left + (right - left) / 2;
+
+            MergeSort(array, left, middle);
+            MergeSort(array, middle + 1, right);
+
+            Merge(array, left, middle, right);
+        }
+    }
+
+    static void Merge(int[] array, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        Array.Copy(array, left, leftArray, 0, n1);
+        Array.Copy(array, middle + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2)
+        {
+            if (leftArray[i] <= rightArray[j])
+            {
+                array[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            array[k] = rightArray[j];
+            j++;
+            k++;
         }
     }
 }
